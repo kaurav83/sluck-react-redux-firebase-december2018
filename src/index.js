63ae3aa5042721f14392dc3aa72839c9056516a3
Vitaliy <puga.vitaliy@gmail.com>
2018-12-24 22:38:34 +0200

    настроил редакс, создал нужный экшен и редюсер, создал компоненты, которые играют роль лайаута

--- src/index.js
+++ src/index.js
@@ -4,23 +4,61 @@ import App from './components/App';
 import Login from './components/Auth/Login';
 import Register from './components/Auth/Rigister';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import firebase from './firebase';
 
-import 'semantic-ui-css/semantic.min.css'
+import {createStore} from 'redux';
+import {Provider, connect} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import rootReducer from './reducers';
+import {setUser} from './actions';
 
-const Root = () => {
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-            </Switch>
-        </Router>
-    )
+import 'semantic-ui-css/semantic.min.css';
+import Spinner from './UI/Spinner';
+
+const store = createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.props.setUser(user);
+                this.props.history.push('/');
+            }
+        })
+    }
+    render() {
+        return this.props.isLoading ? <Spinner /> : (
+            
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            
+        )
+    }
+
 }
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const mapStateToProps = state => {
+    return {
+        isLoading: state.user.isLoading,
+    }
+};
+
+const RootwithAuth = withRouter(
+    connect(mapStateToProps, {setUser})(Root)
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <RootwithAuth />
+        </Router>
+    </Provider>
+    , document.getElementById('root')
+);
 //https://coursehunters.net/course/sozdayte-slack-chat-s-pomoshchyu-react-redux-i-firebase
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
